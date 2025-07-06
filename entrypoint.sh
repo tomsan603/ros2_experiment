c73e21e7c2260bc579f999dc37ea406ee8fe615a
tomsan603 <tomoino139@keio.jp>
2025-07-06 13:28:25 +0900

    add my_robot_moveit_config

--- entrypoint.sh
+++ entrypoint.sh
@@ -14,3 +14,4 @@ sudo chown -R karuto:karuto ~/ros2_ur
 git clone https://github.com/panagelak/rq_fts_ros2_driver.git
 ros2 launch my_robot_description view_model.launch.py
 ros2 launch my_robot_simulation my_robot_sim.launch.py world_file:="/ros2_ws/src/my_packages/my_robot_simulation/worlds/my_world.sdf"
+ros2 launch my_robot_simulation my_robot_sim_moveit.launch.py world_file:="/ros2_ws/src/my_packages/my_robot_simulation/worlds/my_world.sdf"
---
+++ src/my_robot_simulation/launch/my_robot_sim_moveit.launch.py
@@ -0,0 +1,230 @@
+# Copyright (c) 2022 Stogl Robotics Consulting UG (haftungsbeschränkt)
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+#    * Redistributions of source code must retain the above copyright
+#      notice, this list of conditions and the following disclaimer.
+#
+#    * Redistributions in binary form must reproduce the above copyright
+#      notice, this list of conditions and the following disclaimer in the
+#      documentation and/or other materials provided with the distribution.
+#
+#    * Neither the name of the {copyright_holder} nor the names of its
+#      contributors may be used to endorse or promote products derived from
+#      this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+#
+# Author: Denis Stogl
+
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription, OpaqueFunction
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import LaunchConfiguration
+from launch_ros.substitutions import FindPackageShare
+from moveit_configs_utils import MoveItConfigsBuilder
+from launch_ros.actions import Node
+import os
+import xacro
+from ament_index_python.packages import get_package_share_directory
+
+
+def launch_setup(context, *args, **kwargs):
+    # Initialize Arguments
+    ur_type = LaunchConfiguration("ur_type")
+    safety_limits = LaunchConfiguration("safety_limits")
+    # General arguments
+    runtime_config_package = LaunchConfiguration("runtime_config_package")
+    controllers_file = LaunchConfiguration("controllers_file")
+    description_package = LaunchConfiguration("description_package")
+    description_file = LaunchConfiguration("description_file")
+    # moveit_config_package = LaunchConfiguration("moveit_config_package")
+    # moveit_config_file = LaunchConfiguration("moveit_config_file")
+    tf_prefix = LaunchConfiguration("tf_prefix")
+
+    ur_control_launch = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            [FindPackageShare("my_robot_simulation"), "/launch", "/my_robot_sim.launch.py"]
+        ),
+        launch_arguments={
+            "ur_type": ur_type,
+            "safety_limits": safety_limits,
+            "runtime_config_package": runtime_config_package,
+            "controllers_file": controllers_file,
+            "description_package": description_package,
+            "description_file": description_file,
+            "tf_prefix": tf_prefix,
+            "launch_rviz": "false",
+        }.items(),
+    )
+
+    # ur_moveit_launch = IncludeLaunchDescription(
+    #     PythonLaunchDescriptionSource(
+    #         [FindPackageShare("my_robot_moveit_config"), "/launch", "/ur_moveit.launch.py"]
+    #     ),
+    #     launch_arguments={
+    #         "ur_type": ur_type,
+    #         "safety_limits": safety_limits,
+    #         "description_package": description_package,
+    #         "description_file": description_file,
+    #         "moveit_config_package": moveit_config_package,
+    #         "moveit_config_file": moveit_config_file,
+    #         "tf_prefix": tf_prefix,
+    #         "use_sim_time": "true",
+    #         "launch_rviz": "true",
+    #     }.items(),
+    # )
+    moveit_config = (
+        MoveItConfigsBuilder("custom_robot", package_name="my_robot_moveit_config")
+        .robot_description(file_path="config/ur.urdf.xacro")
+        .robot_description_semantic(file_path="config/ur.srdf")
+        .trajectory_execution(file_path="config/moveit_controllers.yaml")
+        .robot_description_kinematics(file_path="config/kinematics.yaml")
+        .planning_scene_monitor(
+            publish_robot_description= True, publish_robot_description_semantic=True, publish_planning_scene=True
+        )
+        .planning_pipelines(
+            pipelines=["ompl", "chomp", "pilz_industrial_motion_planner"]
+        )
+        .to_moveit_configs()
+    )
+    use_sim_time={"use_sim_time": True}
+    config_dict = moveit_config.to_dict()
+    config_dict.update(use_sim_time)
+
+    move_group_node = Node(
+        package="moveit_ros_move_group",
+        executable="move_group",
+        output="screen",
+        parameters=[config_dict],
+        arguments=["--ros-args", "--log-level", "info"],
+    )
+
+    rviz_config_path = os.path.join(
+        get_package_share_directory("my_robot_moveit_config"),
+        "config",
+        "moveit.rviz",
+    )
+
+    rviz_node = Node(
+        package="rviz2",
+        executable="rviz2",
+        name="rviz2",
+        output="screen",
+        arguments=["-d", rviz_config_path],
+        parameters=[
+            moveit_config.robot_description,
+            moveit_config.robot_description_semantic,
+            moveit_config.planning_pipelines,
+            moveit_config.robot_description_kinematics,
+        ],
+    )
+    nodes_to_launch = [
+        ur_control_launch,
+        #ur_moveit_launch,
+        move_group_node,
+        rviz_node
+    ]
+
+    return nodes_to_launch
+
+
+def generate_launch_description():
+    declared_arguments = []
+    # UR specific arguments
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            "ur_type",
+            description="Type/series of used UR robot.",
+            choices=[
+                "ur3",
+                "ur3e",
+                "ur5",
+                "ur5e",
+                "ur7e",
+                "ur10",
+                "ur10e",
+                "ur12e",
+                "ur16e",
+                "ur15",
+                "ur20",
+                "ur30",
+            ],
+            default_value="ur3e",
+        )
+    )
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            "safety_limits",
+            default_value="true",
+            description="Enables the safety limits controller if true.",
+        )
+    )
+    # General arguments
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            "runtime_config_package",
+            default_value="my_robot_simulation",
+            description='Package with the controller\'s configuration in "config" folder. \
+        Usually the argument is not set, it enables use of a custom setup.',
+        )
+    )
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            "controllers_file",
+            default_value="ur3e_controllers_gripper.yaml",
+            description="YAML file with the controllers configuration.",
+        )
+    )
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            "description_package",
+            default_value="my_robot_description",
+            description="Description package with robot URDF/XACRO files. Usually the argument \
+        is not set, it enables use of a custom description.",
+        )
+    )
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            "description_file",
+            default_value="ur_robotiq_ft.urdf.xacro",
+            description="URDF/XACRO description file with the robot.",
+        )
+    )
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            "moveit_config_package",
+            default_value="my_robot_moveit_config",
+            description="MoveIt config package with robot SRDF/XACRO files. Usually the argument \
+        is not set, it enables use of a custom moveit config.",
+        )
+    )
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            "moveit_config_file",
+            default_value="ur.srdf",
+            description="MoveIt SRDF/XACRO description file with the robot.",
+        )
+    )
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            "tf_prefix",
+            default_value='""',
+            description="Prefix of the joint names, useful for \
+        multi-robot setup. If changed than also joint names in the controllers' configuration \
+        have to be updated.",
+        )
+    )
+
+    return LaunchDescription(declared_arguments + [OpaqueFunction(function=launch_setup)])
